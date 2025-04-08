b23c65fab8806d57a8fb92ace64cb033ca6a9dfa
Mariana Souza <marsouzafranca@gmail.com>
2025-04-07 22:35:42 -0300

    Aula 04 de js

---
+++ Aula04/app.js
@@ -0,0 +1,16 @@
+//alert("Hello world!")
+let nome = prompt("Qual o seu nome", "Insira aqui seu nome")
+console.log("Teste console.");
+let resultado = confirm("Você é maior de idade?")
+
+alert(`Olá ${nome}, tudo bem? ${resultado}`)
+alert(resultado)
+
+//``
+//let (valores dinamicos)
+//const (valores que não mudam)
+//let nome 
+//nome = "Mariana"
+//let idade = 20
+//console.log(nome);
+//typeof 
---
+++ Aula04/atividade.js
@@ -0,0 +1,4 @@
+//let nome = prompt("Qual é o seu nome?")
+//let idade = prompt("Qual a sua idade?")
+//let profissao = prompt("Insira qual a sua profissão:")
+//alert(`Olá ${nome} você tem ${idade} e sua profissão é ${profissao}`)
