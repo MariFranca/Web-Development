9e5072fbcb52c281e5b35ce0775b4fef236073e8
Mariana Souza <marsouzafranca@gmail.com>
2025-05-12 22:39:33 -0300

    Aula 06

---
+++ Aula06/app.js
@@ -0,0 +1,25 @@
+//let numero = 0
+//let resposta
+//while(numero<10){
+//console.log(numero)
+//numero++ adiciona u
+//numero--}
+//do{
+//    resposta = prompt('Digite um numero')
+//}
+//while (isNaN(resposta))
+//let numero = prompt('Insira um número inteiro e positivo: ')
+//while(numero > 0){
+//    console.log(numero);
+//    numero--
+//}
+//for(i=0; i<10; i++){
+//    console.log(i);
+//}
+//let numero = 5;
+//console.log(`A tabuada do numero ${numero} é:`)
+//for(i=0; i <=10; i++){
+//    console.log(`O ${numero} x ${i} é igual a ${numero * i}`)
+//}
+
+
